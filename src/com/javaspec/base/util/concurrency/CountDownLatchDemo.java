7732bdbd6a15f4281cdc45ef9cb8f22463b8aa15
Karthigeyan vellasamy <v.karthigeyan@hotmail.com>
2022-07-20 14:34:14 +0530

    Concurrency Init with CountDownLatch

---
+++ src/com/javaspec/base/util/concurrency/CountDownLatchDemo.java
@@ -0,0 +1,59 @@
+package com.javaspec.base.util.concurrency;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchDemo {
+
+	public static void main(String[] args)  {
+
+		CountDownLatch latch = new CountDownLatch(3);
+
+		Worker first = new Worker(1000, latch, "WORKER-1");
+		Worker second = new Worker(2000, latch, "WORKER-2");
+		Worker third = new Worker(3000, latch, "WORKER-3");
+
+		Thread threadOne = new Thread(first);
+		Thread threadtwo = new Thread(second);
+		Thread threadthree = new Thread(third);
+
+		threadOne.start();
+		threadtwo.start();
+		threadthree.start();
+
+		try {
+			System.out.println("Started threads");
+			latch.await();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
+
+class Worker implements Runnable {
+
+	private int delay;
+	private CountDownLatch latch;
+	private String name;
+
+	public Worker(int delay, CountDownLatch latch, String name) {
+		super();
+		this.delay = delay;
+		this.latch = latch;
+		this.name = name;
+	}
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(delay);
+			latch.countDown();
+			System.out.println("Thread : "+ name );
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
