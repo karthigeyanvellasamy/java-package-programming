675a2796b31f88bda1b2931a0d842d1dc7c1085f
Karthigeyan vellasamy <v.karthigeyan@hotmail.com>
2022-08-04 16:28:20 +0530

    Concurrent Programming init

---
+++ src/com/javaspec/base/util/concurrency/RunnableThread.java
@@ -0,0 +1,31 @@
+package com.javaspec.base.util.concurrency;
+
+import java.util.concurrent.TimeUnit;
+
+public class RunnableThread {
+
+	public static void main(String[] args)
+			throws InterruptedException {
+
+		Runnable task = () -> {
+			
+	        try {
+	        	String name =Thread.currentThread().getName();
+				System.out.println("Foo " + name);
+				TimeUnit.SECONDS.sleep(1);
+				System.out.println("Bar " + name);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        
+		};
+		Thread child = new Thread(task);
+		child.start();
+
+		System.out.println(Runtime.getRuntime().availableProcessors());
+		System.out.println(Thread.currentThread().getName());
+		
+	}
+
+}
